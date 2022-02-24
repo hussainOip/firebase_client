c9ece942acd5a6e17aaa4a29b63870f0f9134c16
S.M.Kashif <beastbunny05@gmail.com>
2022-02-25 00:42:29 +0500

    firebase_client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase_client';
+
+  data: Array<any> = [];
+  msg = '';
+
+  searchPosts(e:any){
+    new Promise(async (resolve, reject)=>{
+      const response = await axios.get('http://localhost:5001/testproject-38ed0/us-central1/searchPost?search='+e.target.value);
+      if(response.data.success){
+        resolve(this.data = response.data.data);
+      }else console.log(this.data);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
